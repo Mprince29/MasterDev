4db075594a15b1870a5bd1c80ff19afaa193bef9
Master Prince <prince28.01.2022@gmail.com>
2025-06-26 13:46:02 +0530

    Remove TypeScript namespace block to fix lint error for Vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -538,11 +538,3 @@ export default function Home() {
     </main>
   );
 }
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'spline-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { url: string };
-    }
-  }
-}
